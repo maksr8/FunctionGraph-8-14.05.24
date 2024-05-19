dc4b222ae7d3cf0e43fd90531ed566ae4ae32621
maksr8 <158086853+maksr8@users.noreply.github.com>
2024-05-19 19:25:47 +0300

    add spinners and listener

--- src/main/java/org/example/functiongraph_8_140524/FunctionGraph.java
+++ src/main/java/org/example/functiongraph_8_140524/FunctionGraph.java
@@ -6,22 +6,19 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class FunctionGraph extends Application {
-    private TextField textFieldA = new TextField();
-    private TextField textFieldB = new TextField();
-    private TextField textFieldC = new TextField();
-    private TextField textFieldFrom = new TextField();
-    private TextField textFieldTo = new TextField();
-    private TextField textFieldStep = new TextField();
+    private Spinner<Double> spinnerA;
+    private Spinner<Double> spinnerB;
+    private Spinner<Double> spinnerC;
+    private Spinner<Double> spinnerFrom;
+    private Spinner<Double> spinnerTo;
+    private Spinner<Double> spinnerStep;
     private Button saveButton = new Button("Save");
 
     public static void main(String[] args) {
@@ -45,31 +42,31 @@ public class FunctionGraph extends Application {
         labelParam.setFont(Font.font("Times New Roman", 17.0));
 
         Label labelA = new Label("A");
-        textFieldA = new TextField();
-        GridPane gridPaneA = getGridPaneForLabel(labelA, textFieldA);
+        spinnerA = new Spinner<>(-100.0, 100.0, 3.0, 0.1);
+        GridPane gridPaneA = getGridPaneForLabel(labelA, spinnerA);
 
         Label labelB = new Label("B");
-        textFieldB = new TextField();
-        GridPane gridPaneB = getGridPaneForLabel(labelB, textFieldB);
+        spinnerB = new Spinner<>(-100.0, 100.0, 2.0, 0.1);
+        GridPane gridPaneB = getGridPaneForLabel(labelB, spinnerB);
 
         Label labelC = new Label("C");
-        textFieldC = new TextField();
-        GridPane gridPaneC = getGridPaneForLabel(labelC, textFieldC);
+        spinnerC = new Spinner<>(-5.0, 5.0, 0.82, 0.03);
+        GridPane gridPaneC = getGridPaneForLabel(labelC, spinnerC);
 
         Label labelRange = new Label("Range of t:");
         labelRange.setFont(Font.font("Times New Roman", 17.0));
 
         Label labelFrom = new Label("From");
-        textFieldFrom = new TextField();
-        GridPane gridPaneFrom = getGridPaneForLabel(labelFrom, textFieldFrom);
+        spinnerFrom = new Spinner<>(-100.0, 100.0, -5.0, 0.1);
+        GridPane gridPaneFrom = getGridPaneForLabel(labelFrom, spinnerFrom);
 
         Label labelTo = new Label("To");
-        textFieldTo = new TextField();
-        GridPane gridPaneTo = getGridPaneForLabel(labelTo, textFieldTo);
+        spinnerTo = new Spinner<>(-100.0, 100.0, 5.0, 0.1);
+        GridPane gridPaneTo = getGridPaneForLabel(labelTo, spinnerTo);
 
         Label labelStep = new Label("Step");
-        textFieldStep = new TextField();
-        GridPane gridPaneStep = getGridPaneForLabel(labelStep, textFieldStep);
+        spinnerStep = new Spinner<>(0.1, 10.0, 0.1, 0.1);
+        GridPane gridPaneStep = getGridPaneForLabel(labelStep, spinnerStep);
 
         VBox vBox = new VBox(10, functionLabel, labelParam, gridPaneA, gridPaneB, gridPaneC, labelRange, gridPaneFrom, gridPaneTo, gridPaneStep);
         for (Node child : vBox.getChildren()) {
@@ -89,6 +86,7 @@ public class FunctionGraph extends Application {
         scrollPane.setFitToWidth(true);
         scrollPane.setFitToHeight(true);
         scrollPane.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+
         vBox.setBackground(Background.fill(new Color(0.5, 0.9, 0.9, 0.5)));
         BorderPane borderPane1 = new BorderPane();
         borderPane1.setRight(scrollPane);
@@ -96,13 +94,17 @@ public class FunctionGraph extends Application {
         return new Scene(borderPane1, 1000, 700);
     }
 
-    private GridPane getGridPaneForLabel(Label label, TextField textField) {
+    private GridPane getGridPaneForLabel(Label label, Spinner<Double> spinner) {
         label.setFont(Font.font("Times New Roman", 15.0));
         GridPane.setHalignment(label, HPos.CENTER);
-        label.setLabelFor(textField);
+        label.setLabelFor(spinner);
+        spinner.setEditable(true);
+        spinner.valueProperty().addListener((observable, oldValue, newValue) -> {
+            System.out.println("Spinner value changed from " + oldValue + " to " + newValue);
+        });
         GridPane gridPane = new GridPane();
         gridPane.add(label, 0, 0);
-        gridPane.add(textField, 1, 0);
+        gridPane.add(spinner, 1, 0);
         ColumnConstraints column1 = new ColumnConstraints();
         column1.setPercentWidth(50);
         gridPane.getColumnConstraints().addAll(column1, column1);
